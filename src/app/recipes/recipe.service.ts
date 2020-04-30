58816e413783d1b7384447498846fe77a6a76006
fist22 <hnamanie1@gmail.com>
2020-05-01 01:04:04 +0530

    Fetching Data from Firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,23 +8,29 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome!',
+  //     'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
 
-    new Recipe(
-      'Big Fat Burger',
-      'What elese you need to say?',
-      'https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/fatburger.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'What elese you need to say?',
+  //     'https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/fatburger.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataStorageService {
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService
+  ) {}
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http
+      .put(
+        'https://ng-course-recipe-book-592c5.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-592c5.firebaseio.com/recipes.json'
+      )
+      .subscribe((recipes) => {
+        this.recipesService.setRecipes(recipes);
+      });
+  }
+}
